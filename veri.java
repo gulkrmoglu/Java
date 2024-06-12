62891605f0125c6c91a8bc072ad1280fab6a3fd1
Gül Karamahmutoğlu <156573536+gulkrmoglu@users.noreply.github.com>
2024-06-12 21:08:41 +0300

    Add files via upload

---
+++ veri.java
@@ -0,0 +1,15 @@
+package main;
+
+public class veri {
+
+	public static void main(String[] args) {
+		short i=100;
+		byte j=2;
+		int k=4;
+		
+		long d=i+j+k;
+		System.out.println("long " + d);
+
+	}
+
+}
