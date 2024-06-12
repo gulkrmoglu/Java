4638817b89fe0b370bc3f3a4dffdd2e56f894860
GÜL KARAMAHMUTOGLU <krmoglugul@gmail.com>
2024-06-12 20:45:25 +0300

    gk

---
+++ src/main/mainn.java
@@ -0,0 +1,25 @@
+package main;
+
+public class mainn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=5;
+		
+		int sum=a+b;
+		int difference=a-b;
+		int product=a*b;
+		int quotient=a/b;
+		int remainder=a%b;
+		
+		System.out.println("Sum=" + sum);		
+		System.out.println("Difference=" + difference);
+		System.out.println("Product=" + product);
+		System.out.println("Quotient=" + quotient);
+		System.out.println("Remainder" + remainder);
+		
+	}
+
+}
