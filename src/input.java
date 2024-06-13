2ad0cbe3df5f8c026ae757e28bac055a7c753b56
Gül Karamahmutoğlu <156573536+gulkrmoglu@users.noreply.github.com>
2024-06-13 11:49:39 +0300

    Add files via upload

---
+++ src/input.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Lütfen yaşınızı girin");
+		
+		int yas= scanner.nextInt();
+		
+		System.out.println("Yaşınız: " + yas);		
+		
+	}
+	
+}
